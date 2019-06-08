2b469045ab6f4f77d13329919eca3506849b5ea5
kietpa.ssg <kietpa@ssg.vn>
2019-06-08 10:41:17 +0700

    Acounkit login

---
+++ app/src/main/java/com/example/barberbooking/Common/Common.java
@@ -0,0 +1,5 @@
+package com.example.barberbooking.Common;
+
+public class Common {
+    public  static String IS_LOGIN ="IsLogin";
+}
--- app/src/main/java/com/example/barberbooking/MainActivity.java
+++ app/src/main/java/com/example/barberbooking/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.barberbooking;
 
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
@@ -7,17 +8,95 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Base64;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.barberbooking.Common.Common;
+import com.facebook.accountkit.AccessToken;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitLoginResult;
+import com.facebook.accountkit.ui.AccountKitActivity;
+import com.facebook.accountkit.ui.AccountKitConfiguration;
+import com.facebook.accountkit.ui.LoginType;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import javax.annotation.Nullable;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+    private static final int APP_REQUEST_CODE =7117;
+    @BindView(R.id.btn_login)
+    Button btn_login;
+    @BindView(R.id.txt_Skip)
+    TextView txt_skip;
+    @OnClick(R.id.btn_login)
+    void LoginUser()
+    {
+        final Intent intent = new Intent(this, AccountKitActivity.class);
+        AccountKitConfiguration.AccountKitConfigurationBuilder configurationBuilder =
+                new AccountKitConfiguration.AccountKitConfigurationBuilder(LoginType.PHONE,
+                        AccountKitActivity.ResponseType.TOKEN);
+        intent.putExtra(AccountKitActivity.ACCOUNT_KIT_ACTIVITY_CONFIGURATION,configurationBuilder.build());
+        startActivityForResult(intent, APP_REQUEST_CODE);
+    }
+    @OnClick(R.id.txt_Skip)
+    void  SkipLoginJustGoHome()
+    {
+        Intent intent = new Intent(this, HomeActivity.class);
+        intent.putExtra(Common.IS_LOGIN,false);
+        startActivity(intent);
+
+    }
+    @Override
+    protected  void onActivityResult(int requestCode, int resultcode, @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode,resultcode,data);
+        if(requestCode == APP_REQUEST_CODE)
+        {
+            AccountKitLoginResult accountKitLoginResult = data.getParcelableExtra(AccountKitLoginResult.RESULT_KEY);
+            if(accountKitLoginResult.getError()!= null)
+            {
+                Toast.makeText(this, ""+accountKitLoginResult.getError().getErrorType().getMessage(), Toast.LENGTH_SHORT).show();
+            }
+            else if (accountKitLoginResult.wasCancelled())
+            {
+                Toast.makeText(this, "Login cancelled", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                Intent intent = new Intent(this, HomeActivity.class);
+                intent.putExtra(Common.IS_LOGIN,true);
+                startActivity(intent);
+                finish();
+            }
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        printKeyHash();
+        //
+        AccessToken accessToken = AccountKit.getCurrentAccessToken();
+        if( accessToken != null)
+        {
+            Intent intent = new Intent(this, HomeActivity.class);
+            intent.putExtra(Common.IS_LOGIN,true);
+            startActivity(intent);
+            finish();
+
+        }
+        else
+        {
+            setContentView(R.layout.activity_main);
+            ButterKnife.bind(MainActivity.this);
+        }
+
     }
 
     private void printKeyHash() {
